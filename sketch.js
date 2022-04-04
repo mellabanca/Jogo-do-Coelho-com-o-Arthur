f5aef6cea086580187e2a07747b294ec994463e4
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-04-04 13:12:25 -0300

    04/04/2022

--- sketch.js
+++ sketch.js
@@ -24,6 +24,9 @@ var botal;
 
 var piscando;
 var comendo;
+var triste;
+
+var thau, tek, mimi, nhenhe, suuu;
 
 
 function preload(){
@@ -32,11 +35,20 @@ function preload(){
   tobi = loadImage("Rabbit-01.png");
   piscando = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
   comendo = loadAnimation("eat_0.png","eat_1.png","eat_2.png","eat_3.png","eat_4.png");
+  triste = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
+
+  thau = loadSound("sound1.mp3");
+  tek = loadSound("rope_cut.mp3");
+  mimi = loadSound("sad.wav");
+  nhenhe = loadSound("eating_sound.mp3");
+  suuu = loadSound("air.wav");
 
   piscando.playing = true;
   comendo.playing = true;
+  triste.playing = true;
 
   comendo.loop = false;
+  triste.loop = false;
 }
 
 function setup() 
@@ -63,6 +75,7 @@ function setup()
   coelho.scale = 0.2
   coelho.addAnimation("piscando", piscando);
   coelho.addAnimation("comendo", comendo);
+  coelho.addAnimation("chorando", triste);
   coelho.changeAnimation("piscando");
   botal = createImg("cut_btn.png");
   botal.position(220,30);
@@ -79,8 +92,16 @@ function draw()
 
   piso.exibir();
   corda.show();
-
+  if(colid(ovc,coelho)===true){
+    coelho.changeAnimation("comendo");
+  }
+  if(ovc !==null && ovc.position.y >= 650){
+    coelho.changeAnimation("chorando");
+  }
+  if(ovc!==null){
   image(chocolate, ovc.position.x,ovc.position.y,60,75);
+  }
+  
   drawSprites();
 }
 
@@ -90,4 +111,15 @@ function cai(){
   chave = null;
 }
 
-
+function colid(corpo,Sprite){
+if(corpo!==null){
+  var longe=dist(corpo.position.x,corpo.position.y,Sprite.position.x,Sprite.position.y);
+  if(longe<=80){
+    World.remove(engine.world,ovc);
+    ovc = null;
+    return true;
+  }else{
+    return false;
+  }
+}
+}
